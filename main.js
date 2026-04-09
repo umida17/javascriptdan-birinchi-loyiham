a9ec5d9e6a95b4b08fe9f7c9a8d50e92dac04e96
uy <a.normurzaev20@gmail.com>
2026-04-09 16:20:24 +0500

    data berib olamiz

--- main.js
+++ main.js
@@ -1,1 +1,75 @@
+let container = document.getElementById("container")
 
+
+ let data =[
+  {
+    "id": 1,
+    "title": "Inception",
+    "genre": "Fantastika",
+    "year": 2010,
+    "rating": 8.8
+  },
+  {
+    "id": 2,
+    "title": "Titanic",
+    "genre": "Drama",
+    "year": 1997,
+    "rating": 7.9
+  },
+  {
+    "id": 3,
+    "title": "The Dark Knight",
+    "genre": "Jangari",
+    "year": 2008,
+    "rating": 9.0
+  },
+  {
+    "id": 4,
+    "title": "Interstellar",
+    "genre": "Fantastika",
+    "year": 2014,
+    "rating": 8.6
+  },
+  {
+    "id": 5,
+    "title": "Avengers: Endgame",
+    "genre": "Jangari",
+    "year": 2019,
+    "rating": 8.4
+  },
+  {
+    "id": 6,
+    "title": "Forrest Gump",
+    "genre": "Drama",
+    "year": 1994,
+    "rating": 8.8
+  },
+  {
+    "id": 7,
+    "title": "The Matrix",
+    "genre": "Fantastika",
+    "year": 1999,
+    "rating": 8.7
+  },
+  {
+    "id": 8,
+    "title": "Gladiator",
+    "genre": "Tarixiy",
+    "year": 2000,
+    "rating": 8.5
+  },
+  {
+    "id": 9,
+    "title": "Joker",
+    "genre": "Drama",
+    "year": 2019,
+    "rating": 8.4
+  },
+  {
+    "id": 10,
+    "title": "Avatar",
+    "genre": "Fantastika",
+    "year": 2009,
+    "rating": 7.8
+  }
+]
